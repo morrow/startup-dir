6e2f6fa3e4c898b6efac85222016c539ca56d1c6
Terry Morrow <terrymorrow@gmail.com>
2011-11-05 20:03:51 -0400

    testing

--- compile.rb
+++ compile.rb
@@ -26,8 +26,8 @@ end
 `git add .`
 puts 'enter commit message: '
 message = gets
-`git commit -m '#{message}'
-git push origin master
-git branch -d gh-pages
-git branch gh-pages
-git push origin gh-pages`
+`git commit -m '#{message}'`
+`git push origin master`
+`git branch -d gh-pages`
+`git branch gh-pages`
+`git push origin gh-pages`
