5a4c8e45eecb23d0363b732342eb2c17f38af050
Terry Morrow <terrymorrow@gmail.com>
2011-11-05 20:08:58 -0400

    test

--- compile.rb
+++ compile.rb
@@ -24,10 +24,12 @@ end
 `coffee -cbo javascripts assets/*.coffee`
 
 puts 'enter commit message: '
-message = gets
-`git add .`
-`git commit -m '#{message}'`
-`git push origin master`
-`git branch -d gh-pages`
-`git branch gh-pages`
-`git push origin gh-pages`
+message = gets.strip.chomp
+puts """
+`git add .
+git commit -m '#{message}'
+git push origin master
+git branch -d gh-pages
+git branch gh-pages
+git push origin gh-pages
+"""
