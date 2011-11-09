3bc4c1852f43156340b4c5dbede37d3a4333b3c5
Terry Morrow <terrymorrow@gmail.com>
2011-11-09 01:36:33 -0500

    added link abilty, styling

--- assets/outline.coffee
+++ assets/outline.coffee
@@ -10,13 +10,30 @@ class Outline
     $(element).append(document.createElement("pre")).find("pre").text @htmlify(@outline, true)
 
   tagify:(tag, content="")->
-    return "<#{tag}>#{content}</#{tag}>"
+    if tag and tag.indexOf("(") > 0 and tag.indexOf(")") > 0
+      attributes = tag.split("(")[1].split(")")[0]
+      tag = tag.split("(")[0]
+    else
+      attributes = ""
+    matches = tag.match /a|abbr|address|area|article|aside|audio|b|base|bdi|bdo|blockquote|body|br|button|canvas|caption|cite|code|col|colgroup|command|datalist|dd|del|details|device|dfn|div|dl|dt|em|embed|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|map|mark|menu|meta|meter|nav|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|ul|var|video|wbr/
+    if matches and matches[0].length == tag.length and !matches[0].match /title/
+      return "<#{tag}>#{content}</#{tag}>"
+    else
+      node = "div"
+      if attributes and attributes.match /href/
+        node = "a href='#{content}'"
+
+      return "<#{node} class=\"#{tag}\"#{attributes}>#{content}</#{node}>"
+
+  process:(type="value", input)->
+    return input
 
   htmlify:(object, prettify=false)->
     result = ""
     if object instanceof Array
       for item in object
-        result += @tagify "li", item 
+        item = @process("value", item)
+        result += @tagify "li", item
       result = @tagify "ul", result
     else if object instanceof Object
       for item of object
@@ -30,7 +47,7 @@ class Outline
           when "object"
             result += @tagify tag, @htmlify(object[item])
     else
-      return object
+      return @process("value", object)
     if prettify
-      indent(result)
+      result = indent(result, null)
     return result
--- javascripts/outline.js
+++ javascripts/outline.js
@@ -10,10 +10,32 @@ Outline = (function() {
     return $(element).append(document.createElement("pre")).find("pre").text(this.htmlify(this.outline, true));
   };
   Outline.prototype.tagify = function(tag, content) {
+    var attributes, matches, node;
     if (content == null) {
       content = "";
     }
-    return "<" + tag + ">" + content + "</" + tag + ">";
+    if (tag && tag.indexOf("(") > 0 && tag.indexOf(")") > 0) {
+      attributes = tag.split("(")[1].split(")")[0];
+      tag = tag.split("(")[0];
+    } else {
+      attributes = "";
+    }
+    matches = tag.match(/a|abbr|address|area|article|aside|audio|b|base|bdi|bdo|blockquote|body|br|button|canvas|caption|cite|code|col|colgroup|command|datalist|dd|del|details|device|dfn|div|dl|dt|em|embed|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|map|mark|menu|meta|meter|nav|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|ul|var|video|wbr/);
+    if (matches && matches[0].length === tag.length && !matches[0].match(/title/)) {
+      return "<" + tag + ">" + content + "</" + tag + ">";
+    } else {
+      node = "div";
+      if (attributes && attributes.match(/href/)) {
+        node = "a href='" + content + "'";
+      }
+      return "<" + node + " class=\"" + tag + "\"" + attributes + ">" + content + "</" + node + ">";
+    }
+  };
+  Outline.prototype.process = function(type, input) {
+    if (type == null) {
+      type = "value";
+    }
+    return input;
   };
   Outline.prototype.htmlify = function(object, prettify) {
     var item, result, tag, _i, _len;
@@ -24,6 +46,7 @@ Outline = (function() {
     if (object instanceof Array) {
       for (_i = 0, _len = object.length; _i < _len; _i++) {
         item = object[_i];
+        item = this.process("value", item);
         result += this.tagify("li", item);
       }
       result = this.tagify("ul", result);
@@ -43,10 +66,10 @@ Outline = (function() {
         }
       }
     } else {
-      return object;
+      return this.process("value", object);
     }
     if (prettify) {
-      indent(result);
+      result = indent(result, null);
     }
     return result;
   };
